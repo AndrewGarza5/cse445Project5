6da96fe581f2313df04746527b49eed1dfd73dec
reanna04 <reannaschnabel.rs@gmail.com>
2021-11-28 20:29:20 -0700

    new commit

--- Project5/Staff.aspx
+++ Project5/Staff.aspx
@@ -2,4 +2,34 @@
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
+      <div class="home-wrapper" style="left: 0px; top: 0px; width: 832px; height: 557px">
+        <div class="staff-description">
+            <p style="font-size: 30px;">Welcome Staff Member!</p>
+        </div>
+          <asp:Label ID="Label1" runat="server" style="z-index: 1; position: absolute; top: 314px; left: 24px" Text="Staff Username"></asp:Label>
+          <asp:Label ID="Label2" runat="server" style="z-index: 1; position: absolute; top: 368px; left: 21px" Text="Staff Password"></asp:Label>
+          <asp:Button ID="SubmitStaff" runat="server" OnClick="SubmitStaff_Click" style="z-index: 1; position: absolute; top: 424px; left: 101px" Text="Add Staff" />
+          <asp:TextBox ID="staffUser" runat="server" style="z-index: 1; position: absolute; top: 314px; left: 156px; width: 159px; margin-top: 0;"></asp:TextBox>
+          <asp:TextBox ID="StaffPass" runat="server" style="z-index: 1; position: absolute; top: 368px; left: 152px; width: 162px"></asp:TextBox>
+
+          <asp:Label ID="Label3" runat="server" style="z-index: 1; position: absolute; top: 41px; left: 13px; width: 511px; height: 27px" Text="This page is to add new staff members and to see those who are already added"></asp:Label>
+               </div>
+    <html xmlns="http://www.w3.org/1999/xhtml">
+<body>
+    <form id="form1" runat="server">
+    <div>
+    </div>
+    <asp:XmlDataSource ID="XmlDataSource1" runat="server" 
+        DataFile="https://www.public.asu.edu/~rmschnab/Staffs.xml" 
+        XPath="/WWW/*"></asp:XmlDataSource>
+    <asp:GridView ID="GridView1" runat="server" DataSourceID="XmlDataSource1">
+    </asp:GridView>
+   <asp:TreeView ID="TreeView1" runat="server" DataSourceID="XmlDataSource1">
+    </asp:TreeView>
+    </form>
+</body>
+</html>
+
 </asp:Content>
+
+
--- Project5/Staff.aspx.cs
+++ Project5/Staff.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Project5
 {
@@ -12,6 +15,46 @@ namespace Project5
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void SubmitStaff_Click(object sender, EventArgs e)
+        {
+
+            //create xml file if there isnt one
+            if (!File.Exists("Staff.xml"))
+            {
+                XmlWriterSettings xmlWriterSettings = new XmlWriterSettings();
+                xmlWriterSettings.Indent = true;
+                xmlWriterSettings.NewLineOnAttributes = true;
+                using (XmlWriter xmlWriter = XmlWriter.Create("Staff.xml", xmlWriterSettings))
+                {
+                    xmlWriter.WriteStartDocument();
+                    xmlWriter.WriteStartElement("Staffs");
+
+                    xmlWriter.WriteStartElement("Staff");
+                    xmlWriter.WriteElementString("Username", staffUser.Text);
+                    xmlWriter.WriteElementString("Password", StaffPass.Text);
+                    xmlWriter.WriteEndElement();
+
+                    xmlWriter.WriteEndElement();
+                    xmlWriter.WriteEndDocument();
+                    xmlWriter.Flush();
+                    xmlWriter.Close();
+                }
+            }
+            //append to the existing xml file
+            else
+            {
+                XDocument xDocument = XDocument.Load("Staff.xml");
+                XElement root = xDocument.Element("Staffs");
+                IEnumerable<XElement> rows = root.Descendants("Staff");
+                XElement firstRow = rows.First();
+                firstRow.AddBeforeSelf(
+                   new XElement("Staff",
+                   new XElement("Username", staffUser.Text),
+                   new XElement("Password", StaffPass.Text)));
+                xDocument.Save("Staff.xml");
+            }
         }
     }
 }
